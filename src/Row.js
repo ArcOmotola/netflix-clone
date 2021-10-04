91888b87644ad1a1909a89ac429674216fe75a29
ArcOmotola <ogunsola40@gmail.com>
2021-10-04 02:49:01 +0100

    added youtube popup

--- src/Row.js
+++ src/Row.js
@@ -1,22 +1,49 @@
 import React, {useEffect, useState} from 'react';
 import axios from './axios';  //renamed instance as axios because it was a default export
 import './Row.css';
+import YouTube from 'react-youtube';
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";    //url for image poster
 
 export default function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
 
     useEffect(() => {
         async function fetchData() {
-            const request = await axios.get(fetchUrl);     //e.g https://api.themoviedb.org/3/{fetchUrl} 
-            setMovies(request.data.results);
-            return request;
+            const response = await axios.get(fetchUrl);     //e.g https://api.themoviedb.org/3/{fetchUrl} 
+            setMovies(response.data.results);
+            return response;
         }
         fetchData();
     }, [fetchUrl]);
 
-    console.log(movies);
+    // console.log(movies);
+
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+        }
+    };
+
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl("");
+        } else {
+            movieTrailer(movie?.name || movie?.title || movie?.original_name || "")
+            .then((url) => {
+                //e.g https://www.youtube.com/watch?v=XtMThy8QKqU
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get("v"));
+            })
+            .catch((error) => console.log(error));
+        }
+    };
+
+    console.log("movie trailer>>>",trailerUrl);
 
     return (
         <div className="row">
@@ -26,11 +53,13 @@ export default function Row({ title, fetchUrl, isLargeRow }) {
                     <img 
                         className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                         key={movie.id}
+                        onClick={() => handleClick(movie)}
                         src={`${base_url}${ isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
                         alt={movie.name} 
                     />
                 ))}
             </div>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     )
 }
