ad95fdd860d6ce99f7f09ead98828d8201e358e8
ArcOmotola <ogunsola40@gmail.com>
2021-10-06 09:20:02 +0100

    styled login and signup screens

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import './App.css';
-import Row from './Row';
-import requests from './requests';
-import Banner from './Banner';
-import Nav from './Nav';
+import HomeScreen from './screens/HomeScreen';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
-      <Nav />
-      <Banner />
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+        <Switch>
+          <Route path="/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+        )}
+      </Router>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCTnK0GGcopp0ib23FqLOUimZ-mFZ2seyI",
+    authDomain: "netflix-clone-35ca2.firebaseapp.com",
+    projectId: "netflix-clone-35ca2",
+    storageBucket: "netflix-clone-35ca2.appspot.com",
+    messagingSenderId: "806656623688",
+    appId: "1:806656623688:web:d4167493d587f9ac0e847d",
+    measurementId: "G-XKNXD2922K"
+    };
