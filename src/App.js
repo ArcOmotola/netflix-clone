72b36a20e39a2421f34fa43b77e5273be21a9744
ArcOmotola <ogunsola40@gmail.com>
2021-10-06 11:15:28 +0100

    set up authentication with firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LoginScreen from './screens/LoginScreen';
+import { auth } from './firebase';
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {                                      //listens for user login persistence
+        console.log(userAuth);
+      } else {
+
+      }
+    })
+  }, []);
 
   return (
     <div className="app">
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,7 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyCTnK0GGcopp0ib23FqLOUimZ-mFZ2seyI",
     authDomain: "netflix-clone-35ca2.firebaseapp.com",
@@ -8,3 +11,10 @@ const firebaseConfig = {
     appId: "1:806656623688:web:d4167493d587f9ac0e847d",
     measurementId: "G-XKNXD2922K"
     };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth }
+export default db;
