de69115e302edb517722b48dc1f95fa65c8000b5
ArcOmotola <ogunsola40@gmail.com>
2021-10-06 11:40:53 +0100

    implemented redux

--- src/App.js
+++ src/App.js
@@ -4,19 +4,29 @@ import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LoginScreen from './screens/LoginScreen';
 import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {                                      //listens for user login persistence
-        console.log(userAuth);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
       } else {
-
+        dispatch(logout);
       }
-    })
-  }, []);
+    });
+
+    // return unsubscribe;
+  }, [dispatch]);
 
   return (
     <div className="app">
