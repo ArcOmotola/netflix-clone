a0fa73a0d431fa1e4e45d57d582e71d51956ecc6
ArcOmotola <ogunsola40@gmail.com>
2021-10-07 23:12:54 +0100

    stripe payment integration on plansScreen

---
+++ src/screens/PlansScreen.js
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
+import db from '../firebase';
+import './PlansScreen.css';
+import { loadStripe } from '@stripe/stripe-js';
+
+export default function PlansScreen() {
+    const [products, setProducts] = useState([]);
+    const user = useSelector(selectUser);
+    const [subscription, setSubscription] = useState(null);
+
+    useEffect(() => {
+        db.collection("customers")                                        //to get subscription details from firestore
+        .doc(user.uid)
+        .collection("subscriptions")
+        .get()
+        .then(querySnapshot => {
+            querySnapshot.forEach(async subscription => {
+                setSubscription({
+                    role: subscription.data().role,
+                    current_period_end: subscription.data().current_period_end.seconds,
+                    current_period_start: subscription.data().current_period_start.seconds,
+                })
+            })
+        })
+    }, [user.uid])
+
+    useEffect(() => {
+        db.collection("products")
+        .where("active", "==", true)                                //to get products from firestore
+        .get()
+        .then((querySnapshot) => {
+            const products = {};
+            querySnapshot.forEach(async (productDoc) => {
+                products[productDoc.id] = productDoc.data();
+                const priceSnap = await productDoc.ref.collection("prices").get();
+                priceSnap.docs.forEach(price => {
+                    products[productDoc.id].prices = {
+                        priceId: price.id,
+                        priceData: price.data()
+                    }
+                })
+            });
+            setProducts(products);
+        })
+    }, []);
+
+    console.log("products>>>",products);
+    console.log("subscription>>>",subscription);
+
+    const loadCheckout  = async (priceId) => {
+        const docRef = await db
+            .collection("customers")                                   //gets the user logged in from redux store
+            .doc(user.uid)
+            .collection("checkout_sessions")
+            .add({
+                price: priceId,
+                success_url: window.location.origin,
+                cancel_url: window.location.origin,
+            }); 
+            
+        docRef.onSnapshot(async(snap) => {
+            const {error, sessionId } = snap.data(); 
+            
+            if(error) {
+                //show an error to your customer and
+                //inspect your cloud function logs in the firebase console.
+                alert(`An error occured: ${error.message}`);
+            }
+
+            if (sessionId) {
+                //We have a session, let's redirect to Checkout
+                //Init Stripe
+
+                const stripe = await loadStripe(
+                    "pk_test_51JhkKMLxrBdtcApGadbEeBGzzWyOmLCEoqk6r8WMSa5RUjBJ4CpwXMF2D0865aCFADkS7BVYIwwU4WVz5MfqzgKv00qmkefbfH"
+                );
+                stripe.redirectToCheckout({ sessionId });
+            }
+        })
+    };
+
+    return (
+        <div className="plansScreen">
+            <br />
+            {subscription && <p>Renewal date: {new Date(subscription?.current_period_end * 1000).toLocaleDateString()}</p>}
+            {Object.entries(products).map(([productId, productData]) => {
+               //add some logic to check if user's subscription is active
+                const isCurrentPackage = productData.name?.toLowerCase().includes(subscription?.role);
+
+                return (
+                    <div key={productId} className={`${isCurrentPackage && "plansScreen__plan--disabled"} plansScreen__plan`}>
+                        <div className="plansScreen__info">
+                            <h5>{productData.name}</h5>
+                            <h6>{productData.description}</h6>
+                        </div>
+                        <button onClick={() => !isCurrentPackage && loadCheckout(productData.prices.priceId)}>
+                            {isCurrentPackage? "Current Package" : "Subscribe"}
+                        </button>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { selectUser } from '../features/userSlice';
 import { auth } from '../firebase';
 import Nav from '../Nav';
+import PlansScreen from './PlansScreen';
 import './ProfileScreen.css';
 
 export default function ProfileScreen() {
@@ -19,6 +20,7 @@ export default function ProfileScreen() {
                         <h2>{user.email}</h2>
                         <div className="profileScreen__plans">
                             <h3>Plans</h3>
+                            <PlansScreen />
                             <button 
                                 onClick={() => auth.signOut()}
                                 className="profileScreen__signOut"
